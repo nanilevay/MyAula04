7968f7fe2820e05a96bba61d128b99e6a67190b1
nanilev <dianuski99@gmail.com>
2019-03-22 12:00:58 +0000

    ex1

---
+++ NPCPerks/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NPCPerks
+{
+    enum Class1
+    {
+        
+        Stealth,
+        Combat,
+        Lockpick,
+        Luck
+}
+}
--- NPCPerks/Program.cs
+++ NPCPerks/Program.cs
@@ -6,7 +6,44 @@ namespace NPCPerks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // ask user for number, initialize variable and convert to int
+            Console.WriteLine("How many NPCs do you want?");
+            string number = Console.ReadLine();
+            int num = Convert.ToInt32(number);
+            Class1[] carat;
+       
+
+            Console.WriteLine("Each NPC can have 4 categories:" +
+                "\nStealth \nCombat \nLockpick \nLuck");
+
+            carat = new Class1[num];
+            
+            // ask for the characteristics per NPC
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine($"what characteristics do you want for NPC {i}?");
+                string answer = Console.ReadLine();
+
+                if (answer == "stealth")
+                    carat[i] |= Class1.Stealth;
+
+                if (answer == "combat")
+                    carat[i] |= Class1.Combat;
+
+                if (answer == "lockpick")
+                    carat[i] |= Class1.Lockpick;
+
+                if (answer == "luck")
+                    carat[i] |= Class1.Luck;
+
+            }
+
+            for (int i = 0; i < num; i++)
+            {
+       
+             Console.WriteLine(carat[i]);
+
+            }
         }
     }
 }
